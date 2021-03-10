c72f498a6ec4894141dd73c46a212bed4d50d4e4
pinganyiX <zhouxian@iscas.ac.cn>
2021-03-10 20:55:40 +0800

    help page add twocode img

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -13,12 +13,19 @@
 import React from 'react'
 import './index.less';
 import { connect } from 'react-redux';
-import data from "./../../data/help.json"
+import data from "./../../data/help.json";
+import twocode1 from "./../../img/contact/1.png";
+import twocode2 from "./../../img/contact/2.png";
+import twocode3 from "./../../img/contact/3.png";
+import twocode1en from "./../../img/contact/1en.png";
+import twocode2en from "./../../img/contact/2en.png";
+import twocode3en from "./../../img/contact/3en.png";
 class Help extends React.Component{
     constructor(props){
        super(props)
        this.state ={
-           data
+           data,
+           lf:">>",
            
        }
     }
@@ -40,27 +47,84 @@ class Help extends React.Component{
 
                 </div>
                 <div className="HelpResourceLink content1200">
-                        <div className="helptitle">
+                        <div className="helptitle resourcelink">
                             <div className="indexOneTitle">
-                                <span>{showdata.list[0]}</span>
+                                <span>{showdata.ressourcelink.name}</span>
                             </div>
                             <div className="helpBlueTextLine">
-                            <div className="helpBlueText One" onClick={()=>this.goLink(showdata.link)}><span className="underline">{showdata.list[1]}</span>>></div>
-                            <div className="helpBlueText" onClick={()=>this.goLink(showdata.link)}><span className="underline">{showdata.community}</span>>></div>
-
+                            <div className="helpBlueText One" onClick={()=>this.goLink(showdata.kilink)}><span className="underline">{showdata.ressourcelink.keyissue}</span>{this.state.lf}</div>
+                            <div className="helpBlueText" onClick={()=>this.goLink(showdata.link)}><span className="underline">{showdata.ressourcelink.community}</span>{this.state.lf}</div>
                             </div>
                             
                         </div>
+                        {
+                            this.props.chiFlag === "chi"?
+                            <>
+                            <div className="helptitle">
+                                <div className="indexOneTitle">
+                                    <span>{showdata.contact.name}</span>
+                                </div>
+                                <div className="helpcontact">
+                                    <div className="helpEmail mail">
+                                        <span className="emaillogo"></span>
+                                        <span className="helpemailtext">{showdata.contact.email}:<a href="mailto:summer@iscas.ac.cn">summer@iscas.ac.cn</a></span>
+                                    </div>
+                                    <div className="helpEmail maillist">
+                                        <span className="emaillogo maillist"></span>
+                                        <span className="helpemailtext">{showdata.contact.maillist}:<a href="mailto:summer-ospp@googlegroups.com">summer-ospp@googlegroups.com</a></span>
+                                    </div>
 
-                        <div className="helptitle">
-                            <div className="indexOneTitle">
-                                <span>{showdata.list[2]}</span>
+                                </div>                                                     
                             </div>
-                            <div className="helpEmail mail">
-                                <span className="emaillogo"></span>
-                                <span className="helpemailtext">{showdata.list[3]}:<a href="mailto:summer@iscas.ac.cn">summer@iscas.ac.cn</a></span>
+                            <div className="helpTwoCode">
+                                <div className="helpTwoCodeItem"><img src={twocode1}></img></div>
+                                <div className="helpTwoCodeItem"><img src={twocode2}></img></div>
+                                <div className="helpTwoCodeItem"><img src={twocode3}></img></div>
                             </div>
-                        </div>
+                            </>
+                            :
+                            <div>
+                                <div className="helptitle eng">
+                                    <div className="indexOneTitle">
+                                        <span>{showdata.contact.name}</span>
+                                    </div>
+                                    <div className="helpcontact">
+                                        <div className="helpEmail mail">
+                                            <span className="emaillogo"></span>
+                                            <span className="helpemailtext">{showdata.contact.email}:<a href="mailto:summer@iscas.ac.cn">summer@iscas.ac.cn</a></span>
+                                        </div>
+                                       <div className="helpECListWrapper">
+                                        <div className="helpECList">
+                                                {
+                                                    showdata.contact.emailcontent
+                                                }
+                                            </div>
+                                       </div>
+                                        <div className="helpEmail maillist">
+                                            <span className="emaillogo maillist"></span>
+                                            <span className="helpemailtext">{showdata.contact.maillist}:<a href="mailto:summer-ospp@googlegroups.com">summer-ospp@googlegroups.com</a></span>
+                                        </div>
+                                        <div className="helpECListWrapper maillist">
+                                            {
+                                                showdata.contact.maillistctx.map((item,index)=>{
+                                                    return <div className="helpECList" key={index}><span className="helpEClline" dangerouslySetInnerHTML={{ __html: item}}></span></div>
+                                                })
+                                            }
+                                        </div>
+
+                                    </div>                                                     
+                                </div>
+                                <div className="helpTwoCode">
+                                    <div className="helpTwoCodeItem"><img src={twocode1en}></img></div>
+                                    <div className="helpTwoCodeItem"><img src={twocode2en}></img></div>
+                                    <div className="helpTwoCodeItem"><img src={twocode3en}></img></div>
+                                </div>
+
+                            </div>
+                        }
+                       
+
+                        
                 </div>
 
             </div>
