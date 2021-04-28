02d37a7fed61bf898c41428402048214c241cd6a
pinganyiX <zhouxian@iscas.ac.cn>
2021-04-28 23:28:39 +0800

    add langselect

---
+++ src/components/projectlistN/util.js
@@ -0,0 +1,49 @@
+const getSelectM = function(item){
+    return {
+        "全部":"all",
+        "低":"low",
+        "中":"medium",
+        "高":"high",
+        "All":"all",
+        "Low":"low",
+        "Medium":"medium",
+        "High":"high"
+    }[item]||"all"
+}
+
+const getSelectLang = function(item){   
+    return {
+        "全部":"all",
+        "All":"all",
+        "中文":"chi",
+        "Chinese":"chi",
+        "中文/English":"chien",
+        "Chinese/English":"chien",
+        "English":"eng"
+    }[item]||"all"
+}
+
+const getSelectDToChi = function(tagen){
+    return {
+        "low":"低",
+        "medium":"中",
+        "high":"高"
+    }[tagen]||"全部"
+
+}
+
+const getLangDToChi = function(tagen){
+    return {  
+        "chien":0,
+        "chi":1,
+        "eng":2
+    }[tagen]||0
+}
+
+export {
+    getSelectM,
+    getSelectLang,
+    getSelectDToChi,
+    getLangDToChi
+
+}
